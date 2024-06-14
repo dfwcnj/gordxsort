2be18a39bda7aaab1eb66c5ee57c2b8e8bbb16b8
don caldwell <doncaldwell@dons-Laptop.local>
2024-06-14 15:15:36 -0400

    still a problem at line 73

--- rdxsort.go
+++ rdxsort.go
@@ -3,44 +3,74 @@ package gocrdxsort
 const THRESHOLD int = 1 << 5
 
 type line []byte
+type lines []line
 
-// bostic
-func rsort2a(lines []line, recix int) {
-	var piles = make([][]int, 256)
-	var tlines = make([]line, len(lines))
-	var nc int
-	var ix int = recix
-
-	// iterate over indices in lines
-	for {
-		for i, l := range lines {
-			if ix >= len(l) {
-				continue
-			}
-			c := int(l[ix])
-
-			// aooend offset in line to the pile indexed by c
-			piles[c] = append(piles[c], i)
-			if len(piles[c]) == 1 {
-				nc++ // number of piles so far
-			}
-		}
-		if nc == 0 {
-			return // if no piles, done
-		}
-		for i, _ := range piles {
-			// copy piles to temporary storage in order
-			for j, _ := range piles[i] {
-				tlines = append(tlines, lines[piles[i][j]])
-			}
-			nc--
-			if nc == 0 {
+func simplesort(lns lines) {
+	n := len(lns)
+	if n == 1 {
+		return
+	}
+	var j int
+	for i := 1; i < n; i++ {
+		l := lns[i]
+		for j = i - 1; j >= 0; j-- {
+			// make room
+			if string(lns[j]) > string(l) {
+				lns[j+1] = lns[j]
+			} else {
 				break
 			}
 		}
-		clear(piles)
-		clear(lines)
-		copy(tlines, lines)
-		ix++
+		lns[j+1] = l
+	}
+}
+
+// bostic
+func rsort2a(lns lines, recix int) {
+	var piles = make([][]line, 256)
+	var nc int
+
+	if len(lns) < THRESHOLD {
+		simplesort(lns)
+		return
+	}
+
+	for _, l := range lns {
+
+		if recix >= len(l) {
+			continue
+		}
+
+		// aooend offset in line to the pile indexed by c
+		c := int(l[recix])
+		piles[c] = append(piles[c], l)
+		if len(piles[c]) == 1 {
+			nc++ // number of piles so far
+		}
+	}
+
+	// no lns with offset ix
+	if nc == 0 {
+		return // if no piles, done
+	}
+
+	for i, _ := range piles {
+		if len(piles[i]) == 0 {
+			continue
+		}
+		rsort2a(piles[i], recix+1)
+		nc--
+		if nc == 0 {
+			break
+		}
+	}
+	clear(lns)
+	for i, _ := range piles {
+		if len(piles[i]) == 0 {
+			continue
+		}
+		for _, l := range piles[i] {
+			append(lns, line(l))
+		}
 	}
 }
