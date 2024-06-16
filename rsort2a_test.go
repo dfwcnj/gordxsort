29c1d598566a0792f533de4e6d417eee0304004b
don caldwell <doncaldwell@dons-Laptop.local>
2024-06-15 21:40:46 -0400

    add test for 1<<20

--- rsort2a_test.go
+++ rsort2a_test.go
@@ -10,7 +10,7 @@ func Test_rsort2a(t *testing.T) {
 
 	var lns lines
 	var l uint = 32
-	ls := []uint{1, 2, 16, 128, 65536}
+	ls := []uint{1, 2, 16, 128, 65536, 1 << 20}
 
 	for _, i := range ls {
 
