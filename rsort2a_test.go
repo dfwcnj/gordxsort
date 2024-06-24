8198526b3c59b910410082755fdd3ea590d9e6cb
don caldwell <doncaldwell@dons-Laptop.local>
2024-06-24 10:30:26 -0400

    fix shadowing problem with test

--- rsort2a_test.go
+++ rsort2a_test.go
@@ -11,35 +11,35 @@ func Test_rsort2a(t *testing.T) {
 	var l uint = 32
 	ls := []uint{1, 2, 1 << 4, 1 << 8, 1 << 16, 1 << 20, 1 << 24}
 
-	for _, i := range ls {
+	for _, nl := range ls {
 
 		var lns lines
-		log.Print("testing sort of ", i)
-		rsl := randomstrings(i, l)
-		if len(rsl) != int(i) {
-			log.Fatal("rsl: wanted len ", i, " got ", len(rsl))
+		log.Print("testing sort of ", nl)
+		rsl := randomstrings(nl, l)
+		if len(rsl) != int(nl) {
+			log.Fatal("rsl: wanted len ", nl, " got ", len(rsl))
 		}
 		for _, s := range rsl {
 			bln := []byte(s)
 			lns = append(lns, bln)
 		}
-		if len(lns) != int(i) {
+		if len(lns) != int(nl) {
 			log.Print(lns)
-			log.Fatal("lns: before sort wanted len ", i, " got ", len(lns))
+			log.Fatal("lns: before sort wanted len ", nl, " got ", len(lns))
 		}
 		slns := rsort2a(lns, 0)
-		if len(slns) != int(i) {
+		if len(slns) != int(nl) {
 			//log.Print(slns)
-			log.Fatal("slns: after sort wanted len ", i, " got ", len(slns))
+			log.Fatal("slns: after sort wanted len ", nl, " got ", len(slns))
 		}
 		var ssl []string
 		for _, s := range slns {
 			ssl = append(ssl, string(s))
 		}
 
-		if len(ssl) != int(i) {
+		if len(ssl) != int(nl) {
 			//log.Print(ssl)
-			log.Fatal("ssl: wanted len ", i, " got ", len(ssl))
+			log.Fatal("ssl: wanted len ", nl, " got ", len(ssl))
 		}
 		for i, _ := range ssl {
 			if len(ssl[i]) != int(l) {
@@ -47,9 +47,9 @@ func Test_rsort2a(t *testing.T) {
 			}
 		}
 		if !sort.StringsAreSorted(ssl) {
-			t.Error("rsort2a failed for size ", i)
+			t.Error("rsort2a failed for size ", nl)
 		} else {
-			log.Print("sort test passed for ", i)
+			log.Print("sort test passed for ", nl)
 		}
 	}
 }
